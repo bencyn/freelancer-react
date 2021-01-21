259504cdb85838248f02de5e5f4ee9bfc28722f2
bencyn <bensonnjung39@gmail.com>
2021-01-21 08:25:44 +0300

    first commit

---
+++ src/views/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { Suspense } from 'react'
+
+interface Props {
+
+}
+
+const App: React.FC<Props> = ({}) => {
+        return (
+            <p>Hello world</p>
+        );
+}
+
+export default App
